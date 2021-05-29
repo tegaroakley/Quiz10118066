2a92a5f2f715af1143fcddc229ebdb809018657c
Tegar Oakley <tegaroakley@gmail.com>
2021-05-29 10:47:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quiz10118066/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.quiz10118066;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// Tanggal Pengerjaan : 29 Mei 2021
+// NIM : 10118066
+// Nama : Tegar Lucky Oakley
+// Kelas : IF-2
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
